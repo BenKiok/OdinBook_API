a80cb99638453d9c7960ae5c672b9829007dc8c3
Benedict Kioko <benedictmkioko@gmail.com>
2022-05-08 20:26:14 -0400

    Add router for user signup and login

---
+++ routers/auth.js
@@ -0,0 +1,10 @@
+const express = require('express'),
+      router = express.Router();
+
+// signup
+router.post('/signup', /* controller goes here */);
+
+// login
+router.post('/login', /* controller goes here */);
+
+module.exports = router;
