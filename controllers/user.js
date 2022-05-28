5da76d8f282900a4dc808bcc307857386227dda1
Benedict Kioko <benedictmkioko@gmail.com>
2022-05-28 16:04:22 -0400

    Add controller for friend and friend request endpoints

---
+++ controllers/user.js
@@ -0,0 +1,103 @@
+const { use } = require('passport/lib');
+const User = require('../models/User');
+
+exports.new_friendrequest_PUT = (req, res, next) => {
+  User.findById(req.params.id, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    user.friendRequestsFrom.unshift(req.params.user);
+
+    User.findByIdAndUpdate(user._id, {friendRequestsFrom: user.friendRequestsFrom}, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+
+      return res.json(user);
+    });
+  });
+}
+
+exports.new_friend_PUT = (req, res, next) => {
+  User.findById(req.params.user, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    const requestArr = user.friendRequestsFrom.filter(id => String(id) !== String(req.params.id));
+    user.friends.unshift(req.params.id);
+
+    User.findByIdAndUpdate(user._id, {friendRequestsFrom: requestArr, friends: user.friends}, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+
+      User.findById(req.params.id, (err, friend) => {
+        if (err) {
+          return next(err);
+        }
+
+        friend.friends.unshift(user._id);
+
+        User.findByIdAndUpdate(friend._id, {friends: friend.friends}, (err, friend) => {
+          if (err) {
+            return next(err);
+          }
+
+          return res.json({user, friend});
+        });
+      });
+    });
+  });
+}
+
+exports.remove_friendrequest_PUT = (req, res, next) => {
+  User.findById(req.params.user, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    const requestArr = user.friendRequestsFrom.filter(id => String(id) !== String(req.params.id));
+
+    User.findByIdAndUpdate(user._id, {friendRequestsFrom: requestArr}, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+
+      return res.json(user);
+    });
+  });
+}
+
+exports.remove_friend_PUT = (req, res, next) => {
+  User.findById(req.params.user, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    const friendArr = user.friends.filter(id => String(id) !== String(req.params.id));
+
+    User.findByIdAndUpdate(user._id, {friends: friendArr}, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+
+      User.findById(req.params.id, (err, exfriend) => {
+        if (err) {
+          return next(err);
+        }
+
+        const otherFriendArr = exfriend.friends.filter(id => String(id) !== String(user._id));
+
+        User.findByIdAndUpdate(exfriend._id, {friends: otherFriendArr}, (err, exfriend) => {
+          if (err) {
+            return next(err);
+          }
+
+          return res.json({user, exfriend});
+        });
+      });
+    });
+  });
+}
--- routers/main.js
+++ routers/main.js
@@ -1,16 +1,22 @@
 const express = require('express'),
       router = express.Router(),
       post_controller = require('../controllers/post'),
+      user_controller = require('../controllers/user'),
       comment_controller = require('../controllers/comment');
 
 router.post('/:user/new/post', post_controller.new_post_POST);
+router.post('/:user/new/comment/for_post/:id', comment_controller.new_comment_POST);
+router.put('/:user/new/friendrequestto/:id', user_controller.new_friendrequest_PUT);
+router.put('/:user/new/friend/:id', user_controller.new_friend_PUT);
+router.put('/:user/remove/friendrequestfrom/:id', user_controller.remove_friendrequest_PUT);
+router.put('/:user/remove/friend/:id', user_controller.remove_friend_PUT);
+
 router.put('/post/:id/edit', post_controller.edit_post_PUT);
 router.put('/post/:id/like', post_controller.like_post_PUT);
 router.put('/post/:id/unlike', post_controller.unlike_post_PUT);
 router.delete('/post/:id/delete', post_controller.delete_post_DELETE);
 router.get('/post/:id', post_controller.get_post_GET);
 
-router.post('/:user/new/comment/for_post/:id', comment_controller.new_comment_POST);
 router.put('/comment/:id/edit', comment_controller.edit_comment_PUT);
 router.delete('/comment/:id/delete/from_post/:post', comment_controller.delete_comment_DELETE);
 router.get('/comment/:id', comment_controller.get_comment_GET);
