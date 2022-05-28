f9f37afb4a51d7282bd99c9e02ce4cf3db6d7242
Benedict Kioko <benedictmkioko@gmail.com>
2022-05-28 16:19:02 -0400

    Add likes to comment model

--- models/Comment.js
+++ models/Comment.js
@@ -5,7 +5,8 @@ const commentSchema = new Schema(
   {
     body: {type: String, required: true},
     date: {type: Number, required: true},
-    user: {type: Schema.Types.ObjectId, ref: 'User', required: true}
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    likes: {type: Number, required: true}
   }
 );
 
