877a305ca5b10915b915238ea8fb9e75120316c6
Benedict Kioko <benedictmkioko@gmail.com>
2023-01-30 22:49:00 -0500

    Simplify comment delete endpoint

--- routers/main.js
+++ routers/main.js
@@ -19,7 +19,7 @@ router.delete('/post/:id/delete', post_controller.delete_post_DELETE);
 router.get('/post/:id', post_controller.get_post_GET);
 
 router.put('/comment/:id/edit', comment_controller.edit_comment_PUT);
-router.delete('/comment/:id/delete/from_post/:post', comment_controller.delete_comment_DELETE);
+router.delete('/comment/:id/deletefrom/:post', comment_controller.delete_comment_DELETE);
 router.put('/comment/:id/like', comment_controller.like_comment_PUT);
 router.put('/comment/:id/unlike', comment_controller.unlike_comment_PUT);
 router.get('/comment/:id', comment_controller.get_comment_GET);
